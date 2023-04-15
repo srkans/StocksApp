f09629351ea000f377d3e86fa3c03aaa83f238be
srkans <serkansacma@hotmail.com>
2023-04-15 13:29:14 +0300

    IHttpClientFactory.CreateClient()-using scope

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
---
+++ StocksApp/Services/MyService.cs
@@ -0,0 +1,25 @@
+﻿namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task Method()
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("url"),
+                    Method = HttpMethod.Get
+                };
+
+                 HttpResponseMessage response = await httpClient.SendAsync(httpRequestMessage);
+            }
+        }
+    }
+}
