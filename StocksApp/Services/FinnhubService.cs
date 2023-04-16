62a8beff208442855d7a7c53a3acb47aa17f1e41
srkans <serkansacma@hotmail.com>
2023-04-16 11:53:41 +0300

    getcompanyprofile as service finnhub

--- StocksApp/Services/FinnhubService.cs
+++ StocksApp/Services/FinnhubService.cs
@@ -13,6 +13,38 @@ namespace StocksApp.Services
             _configuration = configuration;
         }
 
+
+        public Dictionary<string,object>? GetCompanyProfile(string stockSymbol)
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}")
+            };
+
+            HttpResponseMessage httpResponseMessage = httpClient.Send(httpRequestMessage);
+
+            string responseBody = new StreamReader(httpResponseMessage.Content.ReadAsStream()).ReadToEnd();
+
+            Dictionary<string,object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string,object>>(responseBody);
+
+            if (responseDictionary == null)
+            { 
+                throw new InvalidOperationException("No response from server");
+            }
+
+            if (responseDictionary.ContainsKey("error"))
+            {
+
+                throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+            }
+
+            return responseDictionary;
+        }
+
         public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
         {
             using (HttpClient httpClient = _httpClientFactory.CreateClient())
