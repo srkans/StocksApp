9a38cfe3630970e2315458797dbe5c23373512e6
srkans <serkansacma@hotmail.com>
2023-04-24 10:43:24 +0300

    Entities, DTOs and contracts has been recreated

--- StocksApp/ServiceContracts/IFinnhubService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StocksApp.ServiceContracts
-{
-    public interface IFinnhubService
-    {
-        Dictionary<string, object>? GetStockPriceQuote(string stockSymbol);
-
-        Dictionary<string, object>? GetCompanyProfile(string stockSymbol);
-    }
-}
